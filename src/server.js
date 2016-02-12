c4719e6d785062fadf955d048d68dfec76569453
Kevin Lanni <therealklanni@gmail.com>
2016-02-11 23:16:21 -0700

    add session middleware

--- src/server.js
+++ src/server.js
@@ -15,11 +15,17 @@ import compose from 'koa-compose'
 // middleware
 import responseTimer from './server/middleware/response-timer'
 import requestLogger from './server/middleware/request-logger'
+import session from 'koa-session'
 import bodyParser from 'koa-bodyparser'
 
 app.use(compose([
   responseTimer(),
   requestLogger(),
+  session(app, {
+    signed: true,
+    secure: true,
+    maxAge: 259200000 // 72 hours
+  }),
   bodyParser()
 ]))
 
