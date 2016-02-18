cfa2f727e35e556294d0bd74e046a284b0d15a6b
Kevin Lanni <therealklanni@gmail.com>
2016-02-18 02:23:28 -0700

    update server keys

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill'
 import path from 'path'
+import { v4 as uuid } from 'uuid'
 import koa from 'koa'
 const app = koa()
 
@@ -9,14 +10,13 @@ const debug = logger('SIR:server')
 import perf from './server/lib/perf'
 const serverStart = perf.start()
 
-app.keys = [process.env.SECRET_TOKEN || 'ohai^^']
+app.keys = [process.env.SECRET_TOKEN1 || uuid(), process.env.SECRET_TOKEN2 || uuid()]
 
 import compose from 'koa-compose'
 
 // middleware
 import responseTimer from './server/middleware/response-timer'
 import requestLogger from './server/middleware/request-logger'
-import session from 'koa-session'
 import bodyParser from 'koa-bodyparser'
 import serve from 'koa-static'
 import mongo from './server/middleware/mongo'
@@ -26,8 +26,8 @@ app.use(compose([
   requestLogger(),
   mongo(),
   session(app, {
+    key: 'sir.sid',
     signed: true,
-    secure: true,
     maxAge: 259200000 // 72 hours
   }),
   bodyParser(),
