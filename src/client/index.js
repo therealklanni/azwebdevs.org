00015d927783ab4d5e3c8d93c0f0bcb3692b8999
Troy Mullaney <troym9731@gmail.com>
2016-03-06 19:14:09 -0700

    Remove unused vars

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,14 @@
 import '../scss/main.scss'
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import MainLayout from './MainLayout'
 import Home from './Home'
 
 render((
   <Router history={browserHistory}>
-    <Router path="/" component={MainLayout}>
+    <Route path="/" component={MainLayout}>
       <IndexRoute component={Home} />
-    </Router>
+    </Route>
   </Router>
 ), document.getElementById('app'))
